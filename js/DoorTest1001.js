497a4a126da550acb67da1471a15dc6da3db9f10
yidianier <yidianier@163.com>
2015-06-04 12:32:23 +0800

    1003

--- js/DoorTest1001.js
+++ js/DoorTest1001.js
@@ -26,10 +26,10 @@ window.onload = function(){
 		clearInterval(timer);
 	};
 	bannerItem.onmouseout = function(){
-		timer = setInterval(toRun,2000);
+		timer = setInterval(toRun,3500);
 	};
 	
-	timer = setInterval(toRun,2000);
+	timer = setInterval(toRun,3500);
 	
 	function toRun(){
 		if(isNow == aLiUl.length-1){
